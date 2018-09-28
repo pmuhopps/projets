06d2ae0272a2adcf28875ec3fe06ebda1646ee29
924421 <glaurent@spir.fr>
2018-09-28 11:56:42 +0200

    Ajout de la selection de la base

--- scr/Projets/Projets.WS/Controllers/TestRedisController.cs
+++ scr/Projets/Projets.WS/Controllers/TestRedisController.cs
@@ -7,7 +7,7 @@ namespace Projets.WS.Controllers
     [Route("api/[controller]")]
     public class TestRedisController : Controller
     {
-        RedisManagerPool manager = new RedisManagerPool("localhost:6379");
+        RedisManagerPool manager = new RedisManagerPool("localhost:6379?db=4");
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
