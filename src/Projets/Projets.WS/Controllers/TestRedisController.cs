a100fd1a68963ce32a7872936333154c07a0b93b
WaKaN <wakan_powa_s01@hotmail.fr>
2018-09-28 13:19:01 +0200

    ajout de docker compose pour redis

--- src/Projets/Projets.WS/Controllers/TestRedisController.cs
+++ src/Projets/Projets.WS/Controllers/TestRedisController.cs
@@ -7,7 +7,8 @@ namespace Projets.WS.Controllers
     [Route("api/[controller]")]
     public class TestRedisController : Controller
     {
-        RedisManagerPool manager = new RedisManagerPool("localhost:6379?db=4");
+        //RedisManagerPool manager = new RedisManagerPool("localhost:6379?db=4");
+        RedisManagerPool manager = new RedisManagerPool("redis:6379?db=4");
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
