1d1bf142497ef5fec9fc2e95a2f050d1f7ba5c75
924421 <wakan_powa_s01@hotmail.fr>
2018-09-28 17:43:54 +0200

    crud groupesdeprojets

---
+++ src/Projets.Bean/BeanGroupeDeProjets.cs
@@ -0,0 +1,9 @@
+﻿namespace Projets.Bean
+{
+    public class BeanGroupeDeProjets
+    {
+        public long Id { get; set; }
+        public string Libelle { get; set; }
+        public bool IsClosed { get; set; }
+    }
+}
---
+++ src/Projets.WS/Controllers/GroupesDeProjetsController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projets.Bean;
+using ServiceStack.Redis;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Projets.WS.Controllers
+{
+    [Route("api/[controller]")]
+    public class GroupesDeProjetsController
+    {
+        RedisManagerPool manager = new RedisManagerPool("localhost:6379?db=4");
+        //RedisManagerPool manager = new RedisManagerPool("redis:6379?db=4");
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            using (var client = manager.GetClient())
+            {
+                var redisBeanGroupeDeProjets = client.As<BeanGroupeDeProjets>();
+                var alls = redisBeanGroupeDeProjets.GetAll();
+                return alls.Select(a => string.Concat("/groupesdeprojets/", a.Id));
+            }
+        }
+        [HttpGet("{id}")]
+        public BeanGroupeDeProjets Get(int id)
+        {
+            using (var client = manager.GetClient())
+            {
+                var redisBeanGroupeDeProjets = client.As<BeanGroupeDeProjets>();
+                var ret = redisBeanGroupeDeProjets.GetById(id);
+                return ret;
+            }
+        }
+        [HttpPost]
+        public void Post([FromBody]BeanGroupeDeProjets value)
+        {
+            using (var client = manager.GetClient())
+            {
+                var redisBeanGroupeDeProjets = client.As<BeanGroupeDeProjets>();
+                value.Id = redisBeanGroupeDeProjets.GetNextSequence();
+                client.Store(value);
+            }
+        }
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]BeanGroupeDeProjets value)
+        {
+            using (var client = manager.GetClient())
+            {
+                var redisBeanGroupeDeProjets = client.As<BeanGroupeDeProjets>();
+                var valdb = redisBeanGroupeDeProjets.GetById(id);
+                valdb.Libelle = value.Libelle;
+                valdb.IsClosed = value.IsClosed;
+                client.Store(valdb);
+            }
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            using (var client = manager.GetClient())
+            {
+                var redisBeanGroupeDeProjets = client.As<BeanGroupeDeProjets>();
+                redisBeanGroupeDeProjets.DeleteById(id);
+            }
+        }
+    }
+}
